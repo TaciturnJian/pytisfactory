294124fc54c18a1501a081d4c4e0ae3fb5421179
三苟 Sango <letusgoone@outlook.com>
2025-08-17 15:27:46 +0800

    测试 mermaid 格式化和几个复合产线

--- app.py
+++ app.py
@@ -5,59 +5,65 @@ Machines = MachineTable().load(Objects)
 
 def mprint(machine: Machine):
     print(machine)
-    print(machine.architect())
+    print(format_architect(machine.architect()))
     print("") #endl
 
 def 强化铁板10(smelter: bool = True):
-    a = Machines["构筑站_螺丝"].multiply(3)
-    b = Machines["构筑站_铁棒"].multiply(2)
+    a = multiple(Machines["构筑站_螺丝"], 3)
+    b = multiple(Machines["构筑站_铁棒"], 2)
     c = Serial("螺丝120", [b, a])
-    d = Machines["构筑站_铁板"].multiply(3)
+    d = multiple(Machines["构筑站_铁板"], 3)
     e = Parallel("强化铁板原料", [c, d])
-    f = Machines["装配站_强化铁板"].multiply(2)
+    f = multiple(Machines["装配站_强化铁板"], 2)
     if smelter:
-        g = Machines["冶炼站_铁锭"].multiply(4)
+        g = multiple(Machines["冶炼站_铁锭"], 4)
         return Serial("强化铁板10", [g, e, f])
     else:
         return Serial("强化铁板10", [e, f])
 
 def 转子8(smelter: bool = True):
-    a = Machines["构筑站_螺丝"].multiply(5)
-    b = Machines("铁棒").multiply(6)
-    c = Serial("转子原料", [b, a])
-    d = Machines("转子").multiply(2)
+    a = multiple(Machines["构筑站_螺丝"], 5)
+    b = multiple(Machines["构筑站_铁棒"], 6)
+    c = Buffer(Item("铁棒", 40))
+    d = Parallel("螺丝及旁路", [a,c])
+    e = multiple(Machines["转子"], 2)
     if smelter:
-        e = Machines["冶炼站_铁锭"].multiply(3)
-        return Serial("转子8", [e, c, d])
+        f = multiple(Machines["冶炼站_铁锭"], 3)
+        return Serial("转子8", [f, b, d, e])
     else:
-        return Serial("转子8", [c, d])
+        return Serial("转子8", [b, d, e])
 
 def 模块化框架20(smelter: bool = True):
-    a = 强化铁板10(smelter=False).multiply(3)
-    b = Machines("铁棒").multiply(8)
+    a = multiple(强化铁板10(smelter=False), 3)
+    b = multiple(Machines["铁棒"], 8)
     c = Parallel("模块化框架原料", [a,b])
-    d = Machines("模块化框架").multiply(10)
+    d = multiple(Machines["模块化框架"], 10)
     if smelter:
-        e = Machines["冶炼站_铁锭"].multiply(16)
+        e = multiple(Machines["冶炼站_铁锭"], 16)
         return Serial("模块化框架20", [e, c, d])
     else:
         return Serial("模块化框架20", [c, d])
     
 def 油田小循环():
-    a = Machines["精炼站_聚合树脂"].multiply(3)
-    b = Machines["抽水站"]
-    c = Parallel("塑料输入", [a, b])
-    d = Machines["聚合塑料"].multiply(6)
-    e = Machines("燃油发电机").multiply(2)
-    return Serial("油田小循环", [c, d, e])
+    a = multiple(Machines["精炼站_聚合树脂"], 3)
+    x1 = Machines["精炼站_残渣燃料油"]
+    x2 = multiple(Machines["燃油发电机"], 2)
+    x = Serial("副产物发电", [x1, x2])
+    y1 = Buffer(Item("聚合树脂", 390, ""))
+    y2 = Machines["抽水站"]
+    y = Parallel("塑料输入", [y1, y2])
+    b = multiple(Machines["精炼站_聚合塑料"], 6)
+    c = Serial("塑料产出", [y,b])
+    d = Parallel("精炼产物消耗", [x, c])
+    return Serial("油田小循环", [a, d])
 
 def test():
-    a = Machines["构筑站_螺丝"].multiply(3)
-    b = Machines("铁棒").multiply(2)
+    a = multiple(Machines["构筑站_螺丝"], 3)
+    b = multiple(Machines["铁棒"], 2)
     return Serial("螺丝120", [b, a])
     
     
 if __name__ == "__main__":
-    a = 模块化框架20()
+    a = 油田小循环()
     mprint(a)
     save_text(mermaid(a), "out/a.mmd")
--- pytisfactory.py
+++ pytisfactory.py
@@ -1,26 +1,66 @@
+# 使用中文注释
+
 from dataclasses import dataclass
 from enum import Enum
 import re
 import logging
 
+#region Code Guard
+
+class CodeGuard:
+    @staticmethod
+    def TypeAssert(obj, expect, message: str|None = None):
+        if not isinstance(obj, expect):
+            if message == None:
+                message = "类型检查失败：期望为{}，实际为{}".format(expect, type(obj))
+            logging.error(message)
+            raise TypeError(message)
+
+#endregion
+
 #region Game Elements
+
+ALLOW_PARALLEL_GROUP_SUBGRAPH = True
+ALLOW_SERIAL_GROUP_SUBGRAPH = True
+ALLOW_MERMAID_COMMENT = False
+
+def FloatStr(v: float) -> str:
+    # 优先整数
+    if v.is_integer():
+        return str(int(v))
+    return str(v)
 
 @dataclass
 class Item:
     """
-    satisfactory 中的一个物品，包含名称、数量和仅用于格式化的名称后缀
+    表示一个具有名称、数量和后缀单位的物品。
+    字符串格式为 "${name}(${count}${postfix})"。
     """
     name: str = "物品"
     count: float = 0.0
     postfix: str = "单位"
     
+    """
+    比较当前 Item 实例与另一个 Item 或字符串是否相等。
+    如果 'other' 是字符串，则通过 parse 将其转换为 Item 实例。
+    只有当 name 和 count 属性都相等时返回 True，否则返回 False。
+    参数:
+        other (Item | str): 要比较的 Item 实例或字符串。
+    返回:
+        bool: 如果两个物品相等返回 True，否则返回 False。
+    """
     def equal(self, other: 'Item|str') -> bool:
         if isinstance(other, str):
-            other = Item(other)
+            temp = Item()
+            if not temp.parse(other):
+                return False
+            other = temp
         
         return self.name == other.name and self.count == other.count
     
-    def has_name(self, name: str) -> bool:
+    def name_equal(self, name: 'Item|str') -> bool:
+        if isinstance(name, Item):
+            name = name.name
         return self.name == name
     
     def simplify(self) -> 'Item':
@@ -29,6 +69,9 @@ class Item:
         return self
     
     def parse(self, text: str) -> bool:
+        self.name = ""
+        self.count = 0.0
+        self.postfix = ""
         text = text.strip()
         if not text:
             return True
@@ -59,20 +102,15 @@ class Item:
         return self.count <= 0 or len(self.name) == 0
     
     def __str__(self) -> str:
-        return "{}({}{})".format(self.name, self.count, self.postfix)
+        return "{}({}{})".format(self.name, FloatStr(self.count), self.postfix)
 
 class Power(Item):
     def __init__(self, count: float = 0):
-        super().__init__(
-            "电力", 
-            count, 
-            "MW")
+        super().__init__("电力", count, "MW")
 
 class ItemList:
-    def __init__(self, items: list[Item] = list()):
-        if not items:
-            items = list()
-        self.items = items        
+    def __init__(self, items: list[Item]|None = None):
+        self.items = [] if items is None else items
         
     def equal(self, other: 'ItemList') -> bool:
         my_len = len(self.items)
@@ -88,7 +126,7 @@ class ItemList:
         if isinstance(item, Item):
             item = item.name
         for my_item in self.items:
-            if my_item.has_name(item):
+            if my_item.name_equal(item):
                 return True
         return False
     
@@ -96,7 +134,7 @@ class ItemList:
         if isinstance(item, Item):
             item = item.name
         for i, my_item in enumerate(self.items):
-            if my_item.has_name(item):
+            if my_item.name_equal(item):
                 return i
         return -1
     
@@ -297,13 +335,6 @@ class Machine:
             inputs=self.inputs.copy(),
             outputs=self.outputs.copy()
         )
-        
-    def multiply(self, factor: float) -> 'Machine':
-        self.name = "{}x{}".format(factor, self.name)
-        self.cost.multiply(factor)
-        self.inputs.multiply(factor)
-        self.outputs.multiply(factor)
-        return self
 
     def clear(self) -> 'Machine':
         self.name = ""
@@ -322,26 +353,53 @@ class Machine:
             self.inputs, 
             self.outputs)
 
+class Buffer(Machine):
+    def __init__(self, item: Item, name: str|None = None):
+        if name is None:
+            name = "{}缓冲".format(item.name)
+        super().__init__(
+            name, 
+            ItemList(),
+            ItemList([item]),
+            ItemList([item]))
+
 class Composition(Enum):
     unknown = "未知"
     serial = "串联"
     parallel = "并联"
 
 class NameIterator:
-    def __init__(self, begin: str = 'a'):
+    def __init__(self, begin: str = 'aaa'): # 至少要三个字符才保证 mermaid 图正常
+        for char in begin:
+            if char < 'a' or char > 'z':
+                begin = 'a'
+                logging.warning("NameIterator 的起始字符({})不在 a-z 范围内，已重置为 '{}'".format(char, begin))
+                break
         self._current = begin
-    
+        logging.info("NameIterator 初始化，起始名称为：{}".format(self.current))
+
+    @property
     def current(self):
         return self._current
     
-    def next(self):
+    def peek(self):
+        """
+        返回下一个名称，但不会推进当前状态。
+        """
         return self._increment(self._current)
-    
+
     def step(self):
+        """
+        将迭代器推进到下一个值，更新内部状态。
+
+        Returns:
+            str: 返回更新后的值。
+        """
         self._current = self._increment(self._current)
         return self._current
     
-    def _increment(self, s):
+    @staticmethod
+    def _increment(s):
         chars = list(s)
         i = len(chars) - 1
         
@@ -356,18 +414,41 @@ class NameIterator:
         return 'a' * (len(s) + 1)
 
 def mermaid_element(name: str, label: str = "") -> str:
+    """
+    生成 mermaid 图节点字符串，格式为 name["label"]。
+    节点是 mermaid 中的基本元素，互相连接构成 mermaid 图。
+
+    Args:
+        name (str): 节点名称。
+        label (str, optional): 节点标签。默认为空字符串。
+
+    Returns:
+        str: 格式化后的 mermaid 节点字符串。
+    """
     name = name.strip()
     label = label.strip()
     if label != "":
-        label = "[\"{}\"]".format(label)
+        label = '["{}"]'.format(label)
     return "{}{}".format(name, label)
 
 def mermaid_link(from_id: str, to_id: str, comment: str|ItemList = "") -> str:
+    """
+    生成 mermaid 图的连接字符串，格式为 from_id-->|"comment"|to_id。
+    用于在 mermaid 图中连接两个节点，并可选添加注释。
+
+    Args:
+        from_id (str): 起始节点的名称。
+        to_id (str): 目标节点的名称。
+        comment (str | ItemList, optional): 连接上的注释内容，可以为字符串或 ItemList。默认为空字符串，也成为 label。
+
+    Returns:
+        str: 格式化后的 mermaid 连接字符串。
+    """
     if isinstance(comment, ItemList):
         comment = str(comment)
     comment = comment.strip()
     if comment != "":
-        comment = "|\"{}\"|".format(comment)
+        comment = '|"{}"|'.format(comment)
     return "{}-->{}{}".format(from_id, comment, to_id)
 
 class MachineList(Machine):
@@ -403,7 +484,8 @@ class MachineList(Machine):
             self.cost.copy(),
             self.inputs.copy(),
             self.outputs.copy(),
-            [machine.copy() for machine in self.machines]
+            [machine.copy() for machine in self.machines],
+            self.composition
         )
         
     def reinit(self) -> 'MachineList':
@@ -474,61 +556,82 @@ class MachineList(Machine):
             logging.warning("无法从字符串({})中解析机器列表数据：{}".format(text, e))
             return False
 
+    @staticmethod
+    def _mermaid_ex_work(begin_name: str, current_name: str, machine: 'MachineList', result: list[str]):
+        inputs = machine.inputs.copy()
+        inputs.pop_power()
+        result.append(mermaid_link(begin_name, current_name, inputs))
+
     def _mermaid_parallel(self, namer: NameIterator) -> str:
-        result = ["subgraph {}".format(self.name)]
-        input_name = namer.current()
+        if not self.machines:
+            return ""
+        result = []
+        if ALLOW_MERMAID_COMMENT:
+            result.append("%% 开始并联 {}".format(self.name))
+        if ALLOW_PARALLEL_GROUP_SUBGRAPH:
+            result.append("subgraph {}".format(self.name))
+        #last_output = namer.current
         parallel_input = namer.step()
-        result.append(mermaid_element(parallel_input, "{}输入".format(self.name)))
-        parallel_inputs = self.inputs.copy()
-        parallel_inputs.pop_power()
-        result.append(mermaid_link(input_name, parallel_input, parallel_inputs))
+        result.append(mermaid_element(parallel_input, "{}并联输入".format(self.name)))
+        #parallel_inputs = self.inputs.copy()
+        #parallel_inputs.pop_power()
+        #result.append(mermaid_link(last_output, parallel_input, parallel_inputs))
         
         name_and_output_list = list()
         for machine in self.machines:
-            current_name = namer.next()
+            current_name = namer.peek()
             if isinstance(machine, MachineList):
                 result.append(machine.mermaid(namer))
+                MachineList._mermaid_ex_work(parallel_input, current_name, machine, result)
             else:
-                namer.step()
+                current_name = namer.step()
                 result.append(mermaid_element(current_name, machine.name))
                 result.append(mermaid_link(parallel_input, current_name, machine.inputs))
-            end_name = namer.current()
+            end_name = namer.current
             name_and_output_list.append((end_name, machine.outputs))
 
         output_name = namer.step()
-        result.append(mermaid_element(output_name, "{}输出".format(self.name)))
+        result.append(mermaid_element(output_name, "{}并联输出".format(self.name)))
         for end_name, outputs in name_and_output_list:
             result.append(mermaid_link(end_name, output_name, outputs))
-        result.append("end")
+        if ALLOW_PARALLEL_GROUP_SUBGRAPH:
+            result.append("end")
+        if ALLOW_MERMAID_COMMENT:
+            result.append("%% 结束并联 {}".format(self.name))
         return '\n\t'.join(result)
     
     def _mermaid_serial(self, namer: NameIterator) -> str:
-        #result = ["subgraph {}".format(self.name)]
-        result = []
-        input_name = namer.current()
+        if not self.machines:
+            return ""
         
-        serial_input = namer.step()
-        result.append(mermaid_element(serial_input, "{}输入".format(self.name)))
-        serial_inputs = self.inputs.copy()
-        serial_inputs.pop_power()
-        result.append(mermaid_link(input_name, serial_input, serial_inputs))
+        result = []
+        if ALLOW_MERMAID_COMMENT:
+            result.append("%% 开始串联 {}".format(self.name))
+        if ALLOW_SERIAL_GROUP_SUBGRAPH:
+            result.append("subgraph {}".format(self.name))
 
+        serial_input = namer.step()
+        result.append(mermaid_element(serial_input, "{}串联输入".format(self.name)))
         last_input = serial_input
 
         for machine in self.machines:
-            current_name = namer.next()
+            current_name = namer.peek()
             if isinstance(machine, MachineList):
                 result.append(machine.mermaid(namer))
+                MachineList._mermaid_ex_work(last_input, current_name, machine, result)
             else:
                 namer.step()
                 result.append(mermaid_element(current_name, machine.name))
                 result.append(mermaid_link(last_input, current_name, machine.inputs))
-            last_input = namer.current()
+            last_input = namer.current
             
         serial_output = namer.step()
-        result.append(mermaid_element(serial_output, "{}输出".format(self.name)))
+        result.append(mermaid_element(serial_output, "{}串联输出".format(self.name)))
         result.append(mermaid_link(last_input, serial_output, self.outputs))
-        #result.append("end")
+        if ALLOW_SERIAL_GROUP_SUBGRAPH:
+            result.append("end")
+        if ALLOW_MERMAID_COMMENT:
+            result.append("%% 结束串联 {}".format(self.name))
         return '\n\t'.join(result)
 
     def mermaid(self, namer: NameIterator) -> str:
@@ -554,9 +657,9 @@ class MachineList(Machine):
             Machine.__str__(self),
             self.composition, 
             ';'.join(str(machine) for machine in self.machines))
-    
+        
 class Parallel(MachineList):
-    def __init__(self, name: str = "并行机器", machines: list[Machine] = list()):
+    def __init__(self, name: str = "并联机器", machines: list[Machine] = list()):
         if not machines:
             machines = list()
         super().__init__(
@@ -580,7 +683,7 @@ class Parallel(MachineList):
         return True
 
 class Serial(MachineList):
-    def __init__(self, name: str = "串行机器", machines: list[Machine] = list()):
+    def __init__(self, name: str = "串联机器", machines: list[Machine] = list()):
         if not machines:
             machines = list()
         super().__init__(
@@ -607,11 +710,39 @@ class Serial(MachineList):
         self.machines = machines.machines
         return True
 
+def multiple(machine: Machine|Parallel|Serial, factor: float) -> Machine:
+    if factor <= 0:
+        return Machine('0x{}'.format(machine.name))
+    
+    if factor.is_integer() and int(factor) == 1:
+        return machine.copy()
+    
+    machines = []
+    value = factor
+    while value > 1:
+        machines.append(machine.copy())
+        value -= 1
+    last = machine.copy()
+    last.inputs.multiply(value)
+    last.outputs.multiply(value)
+    machines.append(last)
+    return Parallel('{}x{}'.format(FloatStr(factor), machine.name), machines)
+
 def mermaid(machine: MachineList) -> str:
-    result = ["graph LR"]
+    result = ["graph TB"]
     namer = NameIterator()
-    result.append(mermaid_element(namer.current(), "开始"))
+    begin_name = namer.current
+    result.append(mermaid_element(begin_name, "开始"))
+    postwork = None
+    if isinstance(machine, MachineList):
+        parallel_input = namer.peek()
+        parallel_inputs = machine.inputs.copy()
+        parallel_inputs.pop_power()
+        postwork = mermaid_link(begin_name, parallel_input, parallel_inputs)
+    
     result.append(machine.mermaid(namer))
+    if postwork is not None:
+        result.append(postwork)
     return '\n\t'.join(result)
 
 #endregion
@@ -768,6 +899,39 @@ def readlines(path: str) -> str:
     except Exception as e:
         logging.warning("无法打开文件并读取({})：{}".format(path, e))
         return ""
+    
+def format_architect(architect: str) -> str:
+    result = ""
+    level: int = 0
+    tab = ' ' * 4
+    last_char = ''
+    for char in architect:
+        if char == '(':
+            level += 1
+            result += char + '\n' + tab * level
+            continue
+        if char == ')':
+            level -= 1
+            if level < 0:
+                raise ValueError("括号不匹配")
+            result += '\n' + tab * level + char
+            continue
+        if char == '|' and last_char == '':
+            last_char = char
+            continue
+        if char == '|' and last_char == '|':
+            last_char = ''
+            result += '||\n' + tab * level
+            continue
+        if char == '-' and last_char == '':
+            last_char = '-'
+            continue
+        if char == '>' and last_char == '-':
+            result += '->\n' + tab * level
+            last_char = ''
+            continue
+        result += char
+    return result
 
 def save_text(text: str, path: str, append: bool = False):
     try:
@@ -785,9 +949,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG, format="[%(levelname)s] %(message)s")
     objs = deserilize(readlines("data.txt"))
     machine_table = MachineTable().load(objs)
-    
-    
-    
     
     save_text(serialize(machine_table.tolist()), "out/out.txt")
     
